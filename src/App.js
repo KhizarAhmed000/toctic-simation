67c2bdd87b078a87301b9aa5f23cb3d0e7cacf5b
KhizarAhmed000 <khizar13ahmed@gmail.com>
2023-10-30 11:51:41 +0500

    test commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Home from './pages/Home'
 // import 'bootstrap/dist/css/bootstrap.min.css';
-
+//comment to test git push new branch
 export default function App() {
   return (
     <div>
